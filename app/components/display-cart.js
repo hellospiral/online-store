dedc73cf0922b48df02f08770b2f589ee49d4127
Bang Nguyen and Matt Carlson <bang+matt@>
2016-08-24 11:45:13 -0700

    add total() method to shopping-cart.js service; display total in display-cart.hbs

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  actions: {
+    displayTotal() {
+      this.get('shoppingCart').total();
+    }
+  }
 });
+
+
+
+
+
+
+
+
+// totalCost: Ember.computed('shoppingCart.items.[]', function() {
+//   var total = 0;
+//   for (var i = 0; i < this.get('shoppingCart.items.length'); i++) {
+//     console.log(this.get('shoppingCart.items'));
+//     var numberAsString = this.get('shoppingCart.items[i].cost');
+//     // var numberAsInt = parseInt(numberAsString.substr(1));
+//     // total += numberAsInt;
+//   }
+//   return total;
--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -3,7 +3,19 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   items: [],
 
+  totalDisplay: 0,
+
   add(item) {
     this.get('items').pushObject(item);
+  },
+
+  total() {
+    var allItems = this.get('items');
+    // console.log(allItems[0].get('cost'));
+    var total = 0;
+    for (var i = 0; i < allItems.length; i++) {
+      total += parseInt(allItems[i].get('cost').substr(1));
+    }
+    this.set('totalDisplay', total);
   }
 });
