9d2f1969fc6f4c61a717cc7ef93f972c930ad8f1
Bang Nguyen and Matt Carlson <bang+matt@>
2016-08-24 10:19:36 -0700

    add cart route; add navbar to application.hbs

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('store');
   this.route('item', {path: '/item/:item_id'});
+  this.route('cart');
 });
 
 export default Router;
